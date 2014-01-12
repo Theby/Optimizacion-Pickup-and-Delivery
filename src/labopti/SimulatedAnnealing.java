4a1d13586d621aac310d4da67e1df7f4793755ee
Esteban Gaete <zoraskiller@gmail.com>
2014-01-12 02:06:42 -0300

    setRequerimientos completada, está el esqueleto de los demás metodos.

--- src/labopti/SimulatedAnnealing.java
+++ src/labopti/SimulatedAnnealing.java
@@ -17,12 +17,26 @@ public class SimulatedAnnealing{
 	public
 		static void setRequerimientos(GrafoTabla grafico, ArrayList<Camion> camiones){
 			double [][] matriz = grafico.getMatriz();
+			int posicion;
+			int num_camiones = camiones.size();
+			int ult_req;
 
-
-
+			while(grafico.getDisponibilidadNodo()>0){
+				for(int i=0;i<camiones;i++){
+					ult_req = camiones[i].getRequerimientos().back();
+					posicion = grafico.getMenorDistanciaDisponible(ult_req);
+					if(posicion != ult_req){
+						camiones[i].setRequerimientos(posicion);
+						camiones[i].setDistanciaRequerimientos(ult_req,posicion);
+						grafico.setDisponibilidadNodo(posicion);
+					}					
+				}
+			}
 		}
 
-		static int heuristica(GrafoTabla grafico){
+		static void setSolucionVecina(GrafoTabla grafico, ArrayList<Camion> camiones);
+
+		static int heuristica(GrafoTabla grafico, ArrayList<Camion> camiones){
 			//valor inicial alto para t
 			//coste_solucion_inicial elegida arbitrariamente
 			ArrayList<NodoTabla> Tabla = grafico.getArray();
@@ -30,7 +44,16 @@ public class SimulatedAnnealing{
                       
 		}
 
-		static 
+		static int getResultadoSolucionObjetivo(ArrayList<Camion> camiones){
+			int size = camiones.size();
+			int solucion = 0;
+
+			for(int i=0;i<size;i++){
+				solucion += camiones.getDistanciaTotal();
+			}
+
+			return solucion;
+		}
 }
 
 
