67ca13a3e8bae3ee4a3cb9a9f3d7642039e7c9ef
Esteban Gaete <zoraskiller@gmail.com>
2014-01-12 22:22:24 -0300

    Privasidad ordenada y dos nuevos metodos para obtener el nodo inicial y destino

--- src/labopti/Requerimiento.java
+++ src/labopti/Requerimiento.java
@@ -9,20 +9,31 @@ package labopti;
  * @author Luis
  */
 public class Requerimiento {
-    MyNodo NodoInicial;
-    MyNodo NodoDestino;
-    int tiempoInicialCarga;
-    int tiempoFinalCarga;
-    int tiempoInicialEntrega;
-    int tiempoFinalEntrega;
+    private
+        MyNodo NodoInicial;
+        MyNodo NodoDestino;
+        int tiempoInicialCarga;
+        int tiempoFinalCarga;
+        int tiempoInicialEntrega;
+        int tiempoFinalEntrega;
     
-    
-    Requerimiento(MyNodo ic, MyNodo ice, int tic, int tfc, int tie, int tfe){
-        NodoInicial = ic;
-        NodoDestino = ice;
-        tiempoInicialCarga = tic;
-        tiempoFinalCarga = tfc;
-        tiempoInicialEntrega = tie;
-        tiempoFinalEntrega = tfe;
-    }
+    public
+        Requerimiento(MyNodo ic, MyNodo ice, int tic, int tfc, int tie, int tfe){
+            NodoInicial = ic;
+            NodoDestino = ice;
+            tiempoInicialCarga = tic;
+            tiempoFinalCarga = tfc;
+            tiempoInicialEntrega = tie;
+            tiempoFinalEntrega = tfe;
+        }
+
+        MyNodo getNodoInicial(){
+            return NodoInicial;
+        }
+
+        MyNodo getNodoDestino(){
+            return NodoDestino;
+        }
+
+
 }
